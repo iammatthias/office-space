0f6bab172e3bc0936e762f7bf0228680e09d1c15
iammatthias <me@iammatthias.com>
2024-12-06 13:49:32 -0800

    Remove "type" field from package.json and lit-html and node-fetch

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import { marked } from "marked";
 import { readFileSync } from "fs";
 import { layout } from "./views/layout";
 import { renderEcosystem } from "./views/ecosystem";
-import fetch from "node-fetch";
 import { EnvironmentalData } from "./types/environmental-data";
 
 const app = new Hono();
@@ -14,7 +13,7 @@ const app = new Hono();
 app.get("/", async (c) => {
   const home = readFileSync(join(__dirname, "content/home.md"), "utf-8");
 
-  // Fetch environment data from the API
+  // Use native fetch
   const apiResponse = await fetch("http://localhost:3000/api/latest");
   const data = await apiResponse.json();
 
