62617a877cb89563fa2c41a2cb55e06f6d5a12e0
iammatthias <me@iammatthias.com>
2025-07-23 22:04:45 -0700

    Integrate OG image setting on page load: Added functionality to set the Open Graph image from the API during the initial render in main.tsx, enhancing social media sharing capabilities.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { setOGImageFromAPI } from "./lib/og-image.ts";
 
-createRoot(document.getElementById('root')!).render(
+// Set the OG image from API on page load
+setOGImageFromAPI().catch(console.error);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
