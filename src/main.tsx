a49903f7bb02cc39b8d9df4fe9ed1788473ee437
iammatthias <me@iammatthias.com>
2025-07-23 22:14:00 -0700

    Fix OG

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { setOGImageFromAPI } from "./lib/og-image.ts";
-
-// Set the OG image from API on page load
-setOGImageFromAPI().catch(console.error);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
